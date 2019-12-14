f0e890a4add64b906d5ab5badb805fb751dc5307
sasaki.h <sasaki.h1026@gmail.com>
2019-12-14 22:50:16 +0900

    インデント整形

--- checker.py
+++ checker.py
@@ -8,50 +8,50 @@ import os
 import re
 
 def check(imgs):
-  target_dir = "./test/"
-  for img in imgs:
-    ## imgタグのリンクをhttpsに変更
-    if 'src' in img.attrs:
-      link = re.sub("^http:", "https:", img['src'] ,1)
-    else :
-      print("src not exists")
-      continue
-    print("check!: " + link)
-    ## 検証
-    try:
-     requests.get(link)
-    except exceptions.SSLError :
-        with open(str(target_dir) + '/link_error' + str('.txt'), mode='a') as file:
-          file.write(str(img['src']) + "\n")
-        continue
-    except Exception :
-      continue
+    target_dir = "./test/"
+    for img in imgs:
+        ## imgタグのリンクをhttpsに変更
+        if 'src' in img.attrs:
+            link = re.sub("^http:", "https:", img['src'] ,1)
+        else :
+            print("src not exists")
+            continue
+        print("check!: " + link)
+        ## 検証
+        try:
+            requests.get(link)
+        except exceptions.SSLError :
+            with open(str(target_dir) + '/link_error' + str('.txt'), mode='a') as file:
+                file.write(str(img['src']) + "\n")
+            continue
+        except Exception :
+            continue
 
 def main():
-  target_url = sys.argv[1]
-  response = ''
-  try:
-    response = requests.get(str(target_url))
-  except exceptions.SSLError:
-    print ("SSLError: " + target_url)
+    target_url = sys.argv[1]
+    response = ''
+    try:
+      response = requests.get(str(target_url))
+    except exceptions.SSLError:
+      print ("SSLError: " + target_url)
 
-  ## 中身チェック
-  soup = BeautifulSoup(response.text, 'html.parser')
-  imgs = soup.select('img')
-  check(imgs)
-  ## 結果を格納するディレクトリ
-  #target = soup.title.string[0:5].strip()
-
-  atags = soup.select('a')
-  for atag in atags:
-    href = atag['href']
-    if (re.match(r"(http:|https)", href) == None):
-      print("continue" + href)
-      continue
-    response = requests.get(href)
+    ## 中身チェック
     soup = BeautifulSoup(response.text, 'html.parser')
-    ## 取得したコンテンツからimgタグを抽出
     imgs = soup.select('img')
     check(imgs)
+    ## 結果を格納するディレクトリ
+    #target = soup.title.string[0:5].strip()
+
+    atags = soup.select('a')
+    for atag in atags:
+        href = atag['href']
+        if (re.match(r"(http:|https)", href) == None):
+          print("continue" + href)
+          continue
+        response = requests.get(href)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        ## 取得したコンテンツからimgタグを抽出
+        imgs = soup.select('img')
+        check(imgs)
 if __name__ == "__main__":
-  main()
+    main()
